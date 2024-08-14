78dfbe1e41b71bdd9bc80a4968c433e3fe197325
Niklas Mårdby <niklas.mardby@gmail.com>
2024-08-14 10:49:48 +0200

    Completed basic game loop

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ The assignment is to clone the repo and open it on your local browser. Check out
 # ⚠️ BAD CODE ⚠️
 
 Warning! The code is intentionally badly written. Hence the name crap-rpg. There should be lots of obvious things to improve on. Also some more subtle. That is the purpose of the repo and the aim of the excersise.
+
+# Suggested issues to fix
+
+There are a lot of big and small things that are bad in the code. Find the ones you find and work at improving. If you are very new to coding then here are some hints what to look at:
+
+-  Look at spacing between components and make sure things are not too tight but spaced cleanly & evenly
+
+-  North, South, East and West buttons could fill the width of the screen each being the same width
+
+-  Remove magic numbers
+
+-  Make sure names of variables are consistent
--- public/app.js
+++ public/app.js
@@ -2,26 +2,55 @@
 
 repo at https://github.com/niklas-mardby/crap-rpg
 
+Please read README.md
+
 */
 
 let playerName = "Test";
 let xCoord = 3;
 let yCoord = 3;
-
-let xMax = 5;
-let xMin = 1;
-let yMax = 5;
-let yMin = 1;
+let gameOn = false;
 
 const newGameButton = document.querySelector("#new-game");
-// const main = document.querySelector("main");
 const game = document.querySelector(".game");
-//const room = document.querySelector("room");
+const room = document.querySelector(".room");
+const gameChildren = game.children;
+const coords = gameChildren[1].children;
+
+document.querySelector("#north").addEventListener("click", (e) => {
+	yCoord--;
+	if (yCoord < 1) yCoord = 1;
+	coords[0].textContent = "x: " + xCoord;
+	coords[1].textContent = "y: " + yCoord;
+	room.textContent = getRoomText();
+});
+document.querySelector("#south").addEventListener("click", (e) => {
+	yCoord++;
+	if (yCoord > 5) yCoord = 5;
+	coords[0].textContent = "x: " + xCoord;
+	coords[1].textContent = "y: " + yCoord;
+	room.textContent = getRoomText();
+});
+document.querySelector("#west").addEventListener("click", (e) => {
+	xCoord--;
+	if (xCoord < 1) xCoord = 1;
+	coords[0].textContent = "x: " + xCoord;
+	coords[1].textContent = "y: " + yCoord;
+	room.textContent = getRoomText();
+});
+document.querySelector("#east").addEventListener("click", (e) => {
+	xCoord++;
+	if (xCoord > 5) xCoord = 5;
+	coords[0].textContent = "x: " + xCoord;
+	coords[1].textContent = "y: " + yCoord;
+	room.textContent = getRoomText();
+});
 
 const savePlayerName = document.querySelector(".player-name button");
 
 newGameButton.addEventListener("click", (e) => {
 	playerName = "";
+	gameOn = true;
 	document.querySelector(".player-name input").value = "";
 
 	document.querySelector(".player-name").style.display = "block";
@@ -37,15 +66,43 @@ savePlayerName.addEventListener("click", (e) => {
 	game.style.display = "block";
 	document.querySelector(".player-name").style.display = "none";
 
-	const gameChildren = game.children;
-	const coords = gameChildren[1].children;
-
 	coords[0].textContent = "x: " + xCoord;
 	coords[1].textContent = "y: " + yCoord;
-
-	gameChildren[2].textContent = getRoomText();
+	room.textContent = getRoomText();
 });
 
 const getRoomText = () => {
-	return "You stand in a dark forest.";
+	const pos = "" + xCoord + yCoord;
+
+	if (pos === "11") return "11";
+	if (pos === "21") return "21";
+	if (pos === "31") return "31";
+	if (pos === "41") return "41";
+	if (pos === "51") return "51";
+
+	if (pos === "12") return "12";
+	if (pos === "22") return "22";
+	if (pos === "32") return "32";
+	if (pos === "42") return "42";
+	if (pos === "52") return "52";
+
+	if (pos === "13") return "13";
+	if (pos === "23") return "23";
+	if (pos === "33") return "33";
+	if (pos === "43") return "43";
+	if (pos === "53") return "53";
+
+	if (pos === "14") return "14";
+	if (pos === "24") return "24";
+	if (pos === "34") return "34";
+	if (pos === "44") return "44";
+	if (pos === "54") return "54";
+
+	if (pos === "15") return "15";
+	if (pos === "25") return "25";
+	if (pos === "35") return "35";
+	if (pos === "45") return "45";
+	if (pos === "55") return "55";
+
+	return "You stand in a dark forest?";
 };
